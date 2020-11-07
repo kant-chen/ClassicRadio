beba74870ba125a124ae5ddce64263534ae9f040
Kant Chen <kant.chen.yan.cheng@gmail.com>
2020-11-07 21:46:57 +0800

    auto-rezie when data is loaded to table

--- classic_radio.py
+++ classic_radio.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file 'classic_radio.ui'
@@ -8,10 +9,10 @@
 
 
 ##########################################
-#程式檔名：classic_radio
-#程式名稱：古典音樂台 - 經典重現
-#程式類別：PyQt5
-#建立日期：2018-12-02
+# 程式檔名：classic_radio
+# 程式名稱：古典音樂台 - 經典重現
+# 程式類別：PyQt5
+# 建立日期：2018-12-02
 ##########################################
 
 from PyQt5.QtCore import *
@@ -20,9 +21,11 @@ from PyQt5.QtWidgets import *
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
-from datetime import  datetime
+from datetime import datetime
 import os
 import webbrowser
+import subprocess
+
 
 class Ui_f_dialog(object):
     def setupUi(self, f_dialog):
@@ -39,7 +42,7 @@ class Ui_f_dialog(object):
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.f_dateEdit = QDateEdit(f_dialog)
-        self.f_dateEdit.setInputMethodHints(Qt.ImhDate|Qt.ImhPreferNumbers)
+        self.f_dateEdit.setInputMethodHints(Qt.ImhDate | Qt.ImhPreferNumbers)
         self.f_dateEdit.setObjectName("f_dateEdit")
         self.horizontalLayout.addWidget(self.f_dateEdit)
         self.f_qry = QPushButton(f_dialog)
@@ -53,7 +56,7 @@ class Ui_f_dialog(object):
         sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        #sizePolicy.setHeightForWidth(self.f_table.sizePolicy().hasHeightForWidth())
+        # sizePolicy.setHeightForWidth(self.f_table.sizePolicy().hasHeightForWidth())
         sizePolicy.setHorizontalPolicy(QSizePolicy.Expanding)
         self.f_table.setSizePolicy(sizePolicy)
         self.f_table.setMinimumSize(QSize(0, 420))
@@ -80,14 +83,9 @@ class Ui_f_dialog(object):
         f_dialog.setTabOrder(self.f_dateEdit, self.f_qry)
         f_dialog.setTabOrder(self.f_qry, self.f_table)
         self.f_dateEdit.setDate(QDate(datetime.now()))
-        print('self.f_dateEdit:', type(self.f_dateEdit))
-        print(self.f_dateEdit)
-        print('datetime.now:',type(QDate(datetime.now())))
-        print(QDate(datetime.now()))
         self.siginal_trigger()
         self.createContextMenu(f_dialog)
-        self.cpp =QApplication.clipboard()
-        
+        self.cpp = QApplication.clipboard()
 
     def retranslateUi(self, f_dialog):
         _translate = QCoreApplication.translate
@@ -99,50 +97,62 @@ class Ui_f_dialog(object):
         item.setText(_translate("f_dialog", "播出時段"))
         item = self.f_table.horizontalHeaderItem(2)
         item.setText(_translate("f_dialog", r"mms連結(雙擊網址直接收聽)"))
-        f_dialog.setWindowIcon(QIcon('music.ico'))
-        #item.setSizeHint(QSize(500,0))
-        #item.setMinimumSize(
-    
+        linux_icon_path = "/usr/share/classic_radio/classic_radio.ico"
+        if os.sys.platform == "linux" and os.path.exists(linux_icon_path):
+            ico_path = linux_icon_path
+        else:
+            ico_path = "music.ico"
+        f_dialog.setWindowIcon(QIcon(ico_path))
+        # item.setSizeHint(QSize(500,0))
+        # item.setMinimumSize(
+
     def qry_url(self):
-        l_datetime = self.f_dateEdit.dateTime().toPyDateTime().strftime('%Y%m%d') #取得存檔時間
+        l_datetime = (
+            self.f_dateEdit.dateTime().toPyDateTime().strftime("%Y%m%d")
+        )  # 取得存檔時間
         l_datetime = str(l_datetime)
-        html = urlopen("http://www1.family977.com.tw/Reprogress_00.asp?strDay=%s#" % l_datetime) 
-        bs = BeautifulSoup(html,'html5lib')
-        soup1 = bs.find_all('td',{'class':'t2-2'})
+        html = urlopen(
+            "http://www1.family977.com.tw/Reprogress_00.asp?strDay=%s#" % l_datetime
+        )
+        bs = BeautifulSoup(html, "html5lib")
+        soup1 = bs.find_all("td", {"class": "t2-2"})
         self.table_init()
         l_i = 0
         for item in soup1:
-            for cell in item.find_all('tr'):
-                #rint(cell.find_all('td')
-                #print(cell.td.text)
+            for cell in item.find_all("tr"):
                 l_list = []
-                #取得節目名稱、節目播放時間
-                for item1 in cell.select('td a')[1:3]:
+                # 取得節目名稱、節目播放時間
+                for item1 in cell.select("td a")[1:3]:
                     if item1.text.strip() is not None:
                         l_list.append(item1.text.strip())
                 if l_list == []:
                     continue
                 try:
-                    #取得mms連結
-                    l_list.append(cell.find('a',{"href":'#'}).attrs['onclick'].split(sep=',')[0]
-                              .split(sep='=')[1][:-1])
+                    # 取得mms連結
+                    l_list.append(
+                        cell.find("a", {"href": "#"})
+                        .attrs["onclick"]
+                        .split(sep=",")[0]
+                        .split(sep="=")[1][:-1]
+                    )
                 except:
                     pass
-                self.f_table.insertRow(l_i)   #新增一列
+                self.f_table.insertRow(l_i)  # 新增一列
                 self.table_insert(l_i, l_list)
                 print(l_list)
                 l_i += 1
-                
+        self.f_table.resizeColumnsToContents()
+
     def siginal_trigger(self):
         QMetaObject.connectSlotsByName(f_dialog)
         self.f_qry.clicked.connect(self.qry_url)
         self.f_table.itemDoubleClicked.connect(self.listen_now)
-    
+
     def table_init(self):
         self.f_table.setRowCount(0)
-        
+
     def table_insert(self, p_ind, p_row):
-        #[列號,col1_vaule,col2_value,col3_value]
+        # [列號,col1_vaule,col2_value,col3_value]
         item0 = QTableWidgetItem()
         item1 = QTableWidgetItem()
         item2 = QTableWidgetItem()
@@ -150,76 +160,81 @@ class Ui_f_dialog(object):
         try:
             str1 = p_row[1]
         except:
-            str1= None
+            str1 = None
         try:
             str2 = p_row[2]
         except:
             str2 = None
         item0.setText(str0)
-        item0.setFlags( Qt.ItemIsSelectable |  Qt.ItemIsEnabled )
+        item0.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
         self.f_table.setItem(p_ind, 0, item0)
         item1.setText(str1)
-        item1.setFlags( Qt.ItemIsSelectable |  Qt.ItemIsEnabled )
+        item1.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
         self.f_table.setItem(p_ind, 1, item1)
         item2.setText(str2)
-        item2.setFlags( Qt.ItemIsSelectable |  Qt.ItemIsEnabled )
+        item2.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
         self.f_table.setItem(p_ind, 2, item2)
-        
+
     def createContextMenu(self, f_dialog):
-        '''
+        """
         创建右键菜单
-        '''
+        """
         # 必须将ContextMenuPolicy设置为Qt.CustomContextMenu
         # 否则无法使用customContextMenuRequested信号
         f_dialog.setContextMenuPolicy(Qt.CustomContextMenu)
         f_dialog.customContextMenuRequested.connect(self.showContextMenu)
- 
+
         # 创建QMenu
         self.contextMenu = QMenu()
-        self.actionA = self.contextMenu.addAction(u'複製到剪貼簿')
+        self.actionA = self.contextMenu.addAction("複製到剪貼簿")
         # 将动作与处理函数相关联
         # 这里为了简单，将所有action与同一个处理函数相关联，
         # 当然也可以将他们分别与不同函数关联，实现不同的功能
         self.actionA.triggered.connect(self.actionHandler)
+
     def actionHandler(self):
-        #菜单中的具体action调用的函数
-        print('菜单中的具体action调用的函数')
+        # 菜单中的具体action调用的函数
         try:
-            #取得當前滑鼠選取cell中的值
+            # 取得當前滑鼠選取cell中的值
             l_current_text = self.f_table.currentItem().text()
         except:
             pass
-        #複製到剪貼簿
+        # 複製到剪貼簿
         self.cpp.setText(l_current_text)
-        print(l_current_text)
+
     def showContextMenu(self, f_dialog):
-        '''
+        """
         右键点击时调用的函数
-        '''
+        """
         # 菜单显示前，将它移动到鼠标点击的位置
         self.contextMenu.popup(QCursor.pos())
+
     def listen_now(self):
         try:
-            l_url = (self.f_table.currentItem().text())
+            l_url = self.f_table.currentItem().text()
         except:
             return
-        if l_url[0:3] == 'mms':
-            print(l_url)
-            webbrowser.open_new_tab(l_url)
-        
-#--------------------- 
-#作者：tinym87 
-#来源：CSDN 
-#原文：https://blog.n.net/tinym87/article/details/6922199 
-#版权声明：本文为博主原创文章，转载请附上博文链接！
+        if l_url[0:3] == "mms":
+            if os.sys.platform == "linux":
+                cmd = "vlc {}".format(l_url)
+                subprocess.Popen(cmd, shell=True)
+            else:
+                webbrowser.open_new_tab(l_url)
+
+
+# ---------------------
+# 作者：tinym87
+# 来源：CSDN
+# 原文：https://blog.n.net/tinym87/article/details/6922199
+# 版权声明：本文为博主原创文章，转载请附上博文链接！
 
 
 if __name__ == "__main__":
     import sys
+
     app = QApplication(sys.argv)
     f_dialog = QDialog()
     ui = Ui_f_dialog()
     ui.setupUi(f_dialog)
     f_dialog.show()
     sys.exit(app.exec_())
-
